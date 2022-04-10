c7ef5eeb102c9043b55a9da921b95874278dc922
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-04-10 14:13:30 -0300

    Observando mudanças

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { timeToSeconds } from '../../common/utils/time'
 import { Task } from '../../types/task'
 import { Button } from '../Button'
@@ -14,9 +14,11 @@ export function Stopwatch(props: StopwatchProps) {
   const { select } = props
   const [time, setTime] = useState<number>(0)
 
-  if (select?.tempo) {
-    setTime(timeToSeconds(select.tempo))
-  }
+  useEffect(() => {
+    if (select?.tempo) {
+      setTime(timeToSeconds(select.tempo))
+    }
+  }, [select?.tempo])
 
   return (
     <div className={style.cronometro}>
